fb7f865da0440f282421997918e2bc90382f2b05
Chaoci <charlieischao@gmail.com>
2022-07-06 14:36:58 +0800

    Update my.js

--- js/my.js
+++ js/my.js
@@ -1,21 +1,22 @@
 const mean = document.querySelector('.menu');
 var div = document.querySelector("i");
 var cssCollection = window.getComputedStyle(div, null);
-var result = cssCollection.getPropertyValue("display");
-console.log(result);
+var result = cssCollection.getPropertyValue("display"); //利用js取css值
 
 $(function () {
 	$(".menu i").click(function (e) {
 		e.preventDefault();
 		//h2 在父層元素中，找到 p 元素，並且給它滑動效果
-		$('.slide').parent().find("li").slideToggle();
+		$('.slide').parent().find("li").slideToggle(600);
 		//h2 在父層 .question 元素中，找到其他 .question 同層元素，再找到該同層元素的 p 標籤，並向上收闔
-		$('.slide').parent().siblings().find("li").slideUp();
+		$('.slide').parent().siblings().find("li").slideUp(600);
 	});
-	if(result == 'block'){
+
+	//如果有出現menu icon則為block，表示正在手機畫面則執行以下code
+	if(result == 'block'){  
 		$(".slide li").click(function(e){
-			$('.slide').parent().find("li").slideToggle(800);
-			$('.slide').parent().siblings().find("li").slideUp(800);
+			$('.slide').parent().find("li").slideToggle(600);
+			$('.slide').parent().siblings().find("li").slideUp(600);
 		})
 	}
 });
