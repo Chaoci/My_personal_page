3fbc3fd87221b60df1e9db59213624f36a6ad546
Chaoci <charlieischao@gmail.com>
2022-07-06 14:57:34 +0800

    rwd

--- js/my.js
+++ js/my.js
@@ -7,16 +7,16 @@ $(function () {
 	$(".menu i").click(function (e) {
 		e.preventDefault();
 		//h2 在父層元素中，找到 p 元素，並且給它滑動效果
-		$('.slide').parent().find("li").slideToggle(600);
+		$('.slide').parent().find("li").slideToggle(700);
 		//h2 在父層 .question 元素中，找到其他 .question 同層元素，再找到該同層元素的 p 標籤，並向上收闔
-		$('.slide').parent().siblings().find("li").slideUp(600);
+		$('.slide').parent().siblings().find("li").slideUp(700);
 	});
 
 	//如果有出現menu icon則為block，表示正在手機畫面則執行以下code
 	if(result == 'block'){  
 		$(".slide li").click(function(e){
-			$('.slide').parent().find("li").slideToggle(600);
-			$('.slide').parent().siblings().find("li").slideUp(600);
+			$('.slide').parent().find("li").slideToggle(700);
+			$('.slide').parent().siblings().find("li").slideUp(700);
 		})
 	}
 });
